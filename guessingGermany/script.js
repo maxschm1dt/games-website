1db6cf1e7db7db4036b52ab7b55c6f3c71a57c79
Max Schmidt <to.maxschmidt@pm.me>
2024-02-18 11:24:48 +0100

    added landkreise to leaflet

--- guessingGermany/script.js
+++ guessingGermany/script.js
@@ -1,7 +1,17 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+import data from "./landkreise_simplify200.geojson" assert { type: "json" };
+
+var map = L.map("map").setView([49.79258938006419, 9.932129693357913], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+function onEachFeature(feature, layer) {
+  var pop = layer.bindPopup(
+    "" + feature.properties.GEN + " (" + feature.properties.BEZ + ")"
+  );
+}
+
+L.geoJSON(data, { onEachFeature: onEachFeature }).addTo(map);
